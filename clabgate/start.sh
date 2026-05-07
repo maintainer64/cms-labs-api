4d4502b52bee8fedaf5e1792082ebde95ffece5e
maintainer64 <20949800+maintainer64@users.noreply.github.com>
2026-05-08 00:59:03 +0500

    fix: start

--- clabgate/start.sh
+++ clabgate/start.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+ENV_DIR=/var/run/secrets/app
 set -e;
+if [ -d "$ENV_DIR" ]; then
+  echo "Copy file env"
+  cp "$ENV_DIR/env" /app/.env
+fi
 echo "Start web server"
-/app/clabgate
+/app/clabgate $*
